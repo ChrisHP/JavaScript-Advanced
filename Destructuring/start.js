889c88225c603abd1452e6726cfd60781619fbaf
Christian <christianalexp@live.com>
2024-01-05 22:27:55 +0100

    Ej22

--- Destructuring/start.js
+++ Destructuring/start.js
@@ -1,10 +1,13 @@
-let num1 = 10;
-let num2 = 20;
-console.log('Before swap: ', num1, num2); // Before swap: 10 20
-
-// Variable swapping
-let temp = num2;
-num2 = num1;
-num1 = temp;
-
-console.log('After swap: ', num1, num2); // After swap: 20 10
+const person = {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  };
+  
+  const id = person.id;
+  const firstName = person.firstName;
+  const lastName = person.lastName;
+  const age = person.age;
+  
+  console.log(id, firstName, lastName, age);
