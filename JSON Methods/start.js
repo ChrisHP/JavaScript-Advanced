7651440b8c806edc6bf0cb322b12b55879502d0b
Christian <christianalexp@live.com>
2024-01-06 19:17:17 +0100

    Ej30

--- JSON Methods/start.js
+++ JSON Methods/start.js
@@ -1,16 +1,20 @@
 class Person {
-    constructor(id, firstName, lastName, age) {
-      this.id = id;
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.age = age;
-    }
-  
-    toJson() {
-      return JSON.stringify(this);
-    }
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
   }
-  
-  const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
-  const developer = Person.fromJson(json);
-  console.log(developer);
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+
+  static fromJson(j) {
+    return new Person( ...Object.values(JSON.parse(j)));
+  }
+}
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.fromJson(json);
+console.log(developer);
