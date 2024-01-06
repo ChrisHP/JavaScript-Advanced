1e500bdd5c8066362758d00eb76c8b0f954013c8
Christian <christianalexp@live.com>
2024-01-06 19:36:41 +0100

    Ej32

--- JSON Methods/start.js
+++ JSON Methods/start.js
@@ -1,10 +1,18 @@
 const person = {
-    id: 1,
-    firstName: 'Mario',
-    lastName: 'Rossi',
-    age: 25,
-  };
-  
-  const json = JSON.stringify(person);
-  
-  console.log(json); // Should return: { "id": 1, "age": 25 }
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+//Forma 1
+//   const json = JSON.stringify(person,["id","age"]);
+
+//Forma 2
+const json = JSON.stringify(person, (a, b) => {
+  if (a == "firstName" || a == "lastName") {
+    return undefined;
+  }
+  return b;
+});
+
+console.log(json); // Should return: { "id": 1, "age": 25 }
