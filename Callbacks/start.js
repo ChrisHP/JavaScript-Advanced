9cf2f230c7babb66d737e3f5b1af41eac0af5f3c
Christian <christianalexp@live.com>
2024-01-10 14:37:26 +0100

    Ej36

--- Callbacks/start.js
+++ Callbacks/start.js
@@ -1,5 +1,7 @@
-function repeatHello(cbFunction){
-setInterval(cbFunction, 1000);
+function repeatHello(cbFunction) {
+  const id = setInterval(cbFunction,1000);
+  setTimeout(()=>clearInterval(id),5000);
 }
-
-repeatHello(()=>console.log("Hello"));
+//La funcion callback debe ser una arrow function 
+//porque al hacer un callback no acepta parametros
+repeatHello(() => console.log("Hello"));
