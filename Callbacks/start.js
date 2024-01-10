7fc551be4113932efb121c02e297d9a985df4b73
Christian <christianalexp@live.com>
2024-01-10 14:28:26 +0100

    Ej35

--- Callbacks/start.js
+++ Callbacks/start.js
@@ -1,6 +1,5 @@
-function printAsyncName(cbFunction, name){
-setTimeout(cbFunction,1000);
-setTimeout(()=>console.log(name),2000)
+function repeatHello(cbFunction){
+setInterval(cbFunction, 1000);
 }
 
-printAsyncName(()=>console.log("Hello"),"Christian");
+repeatHello(()=>console.log("Hello"));
